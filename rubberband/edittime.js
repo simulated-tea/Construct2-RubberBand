bdc59ac3f421f49fb3abc6639ced2e12780905e6
Simon <t-sim@gmx.net>
2015-01-23 20:45:00 +0100

    fixing that stupid missing comma for good

--- rubberband/edittime.js
+++ rubberband/edittime.js
@@ -34,7 +34,7 @@ var property_list = [
     new cr.Property(ept_integer, "Relaxed Length", 100,  "The distance allowed before any effect is felt in pixel"),
     new cr.Property(ept_float, "Stiffness", 5, "The stength of the force if stretched"),
     new cr.Property(ept_float, "Gravity", 10, "Optional gravity effect, in pixel/second"),
-    new cr.Property(ept_float, "Drag", 1, "Optional drag effect")
+    new cr.Property(ept_float, "Drag", 1, "Optional drag effect"),
     new cr.Property(ept_combo, "Initial State", "Enabled", "Whether to initially have the behavior enabled or disabled", "Disabled|Enabled")
 ];
 
